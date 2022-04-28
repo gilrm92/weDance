b75dc6312205a6f473bb3a859cf448466f2491e3
Gilberto <gilbertorm.ramos@gmail.com>
2022-04-28 10:08:30 +0200

    forgot one little thing

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from controllers import userController, studentsController
+from controllers import userController, studentsController, loginController
 
 router = APIRouter()
 router.include_router(userController.router)
 router.include_router(studentsController.router)
+router.include_router(loginController.router)
